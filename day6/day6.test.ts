1f6f28e7f9e1fb6055c6b85921a79218694d16ef
Wayne Crouch <wgcrouch@users.noreply.github.com>
2024-12-06 10:23:52 +0000

    day 6 part 1

--- day6/day6.test.ts
+++ day6/day6.test.ts
@@ -13,18 +13,18 @@ const day6Example1 = `....#.....
 #.........
 ......#...`;
 
-test.only("day 6 part 1 example 1", () => {
-  expect(day6part1(day6Example1)).toEqual(143);
+test("day 6 part 1 example 1", () => {
+  expect(day6part1(day6Example1)).toEqual(41);
 });
 
 test("day 6 part 1 real", () => {
   expect(day6part1(day6input)).toEqual(4774);
 });
 
-test("day 6 part 2 example", () => {
-  expect(day6part2(day6Example1)).toEqual(123);
-});
+// test("day 6 part 2 example", () => {
+//   expect(day6part2(day6Example1)).toEqual(123);
+// });
 
-test("day 6 part 2 real", () => {
-  expect(day6part2(day6input)).toEqual(6004);
-});
+// test("day 6 part 2 real", () => {
+//   expect(day6part2(day6input)).toEqual(6004);
+// });
--- day6/day6.ts
+++ day6/day6.ts
@@ -1,4 +1,4 @@
-import { findInGrid, Grid, N, XY } from "../utils/grid";
+import { findInGrid, Grid, N, E, S, W, XY, move } from "../utils/grid";
 
 function parseInput(input: string): Grid<string> {
   return input.split("\n").map((x) => x.split(""));
@@ -6,9 +6,27 @@ function parseInput(input: string): Grid<string> {
 
 export function day6part1(input: string) {
   const grid = parseInput(input);
-  let currentDir = N;
+  let dir = N;
   let current: XY | undefined = findInGrid(grid, "^");
-  while (current) {}
+
+  if (!current) {
+    return 0;
+  }
+  let visited = new Set<string>();
+
+  while (current) {
+    visited.add(current.join(","));
+    const next: XY | undefined = move(grid, current, dir);
+    if (!next) {
+      break;
+    }
+    if (grid[next[1]][next[0]] === "#") {
+      dir = dir === N ? E : dir === E ? S : dir === S ? W : dir === W ? N : N;
+    } else {
+      current = next;
+    }
+  }
+  return visited.size;
 }
 
 export function day6part2(input: string) {}
